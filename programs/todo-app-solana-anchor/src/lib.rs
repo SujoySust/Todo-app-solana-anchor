a6e406c1b4dd91eb6615aa34e0ee2a2173ea95ed
Sujoy Nath <sujoy.csesust@gmail.com>
2024-04-01 07:46:42 +0600

    add all functions

--- programs/todo-app-solana-anchor/src/lib.rs
+++ programs/todo-app-solana-anchor/src/lib.rs
@@ -1,15 +1,214 @@
 use anchor_lang::prelude::*;
 
+pub mod constant;
+pub mod error;
+pub mod states;
+
+use crate::{constant::*, error::*, states::*};
+
 declare_id!("EqaKtMpHuhqRbakvfMGoLXHZsV6dCD3DRp6uqEU5pbVq");
 
 #[program]
 pub mod todo_app_solana_anchor {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_user(ctx: Context<InitializeUser>) -> Result<()> {
+        // Initialize user profile with default data
+        let user_profile = &mut ctx.accounts.user_profile;
+        user_profile.authority = ctx.accounts.authority.key();
+        user_profile.last_todo = 0;
+        user_profile.todo_count = 0;
+
         Ok(())
     }
+
+    pub fn create_todo(ctx: Context<CreateTodo>, _content: String) -> Result<()> {
+        let todo_account = &mut ctx.accounts.todo_account;
+        let user_profile = &mut ctx.accounts.user_profile;
+
+        // Fill the contents with argument
+        todo_account.authority = ctx.accounts.authority.key();
+        todo_account.idx = user_profile.last_todo;
+        todo_account.content = _content;
+        todo_account.marked = false;
+
+        // Increase todo idx for PDA
+        user_profile.last_todo = user_profile.last_todo
+            .checked_add(1)
+            .unwrap();
+
+        // Increase total todo count
+        user_profile.todo_count = user_profile.todo_count
+            .checked_add(1)
+            .unwrap();
+
+        Ok(())
+    }
+
+    pub fn update_todo(ctx: Context<UpdateTodo>, _todo_idx: u8, _content: String) -> Result<()> {
+        let todo_account = & mut ctx.accounts.todo_account;
+        // Update todo
+        todo_account.content = _content;
+        Ok(())
+    }
+
+    pub fn delete_todo(ctx: Context<DeleteTodo>, _todo_idx: u8) -> Result<()> {
+        // Decrease total todo count
+        let user_profile = &mut ctx.accounts.user_profile;
+        user_profile.todo_count = user_profile.todo_count
+            .checked_sub(1)
+            .unwrap();
+        // No need to decrease last todo idx
+        // Todo PDA already closed in context
+        Ok(())
+    }
+
+    pub fn mark_todo(ctx: Context<MarkTodo>, _todo_idx:u8) -> Result<()> {
+        let todo_account = &mut ctx.accounts.todo_account;
+        require!(!todo_account.marked, TodoError::AlreadyMarked);
+
+        //Mark todo
+        todo_account.marked = true;
+        Ok(())
+    }
+
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction()]
+pub struct InitializeUser<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        init,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = 8 + std::mem::size_of::<UserProfile>()
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction()]
+pub struct CreateTodo<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        has_one = authority
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    #[account(
+        init,
+        seeds = [TODO_TAG, authority.key().as_ref(), &[user_profile.last_todo as u8].as_ref()],
+        bump,
+        payer = authority,
+        space = std::mem::size_of::<TodoAccount>() + 8
+    )]
+    pub todo_account: Box<Account<'info, TodoAccount>>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(todo_idx: u8)]
+pub struct UpdateTodo<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        has_one = authority
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    #[account(
+        mut,
+        seeds = [TODO_TAG, authority.key().as_ref(), &[todo_idx].as_ref()],
+        bump,
+        has_one = authority
+    )]
+    pub todo_account: Box<Account<'info, TodoAccount>>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(todo_idx: u8)]
+pub struct DeleteTodo<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        has_one = authority
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    #[account(
+        mut,
+        close = authority,
+        seeds = [TODO_TAG, authority.key().as_ref(), &[todo_idx].as_ref()],
+        bump,
+        has_one = authority,
+    )]
+    pub todo_account: Box<Account<'info, TodoAccount>>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(todo_idx: u8)]
+pub struct MarkTodo<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        has_one = authority
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    #[account(
+        mut,
+        close = authority,
+        seeds = [TODO_TAG, authority.key().as_ref(), &[todo_idx].as_ref()],
+        bump,
+        has_one = authority,
+    )]
+    pub todo_account: Box<Account<'info, TodoAccount>>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn is_zero_account(account_info: &AccountInfo) -> bool {
+    let account_data: &[u8] = &account_info.data.borrow();
+    let len = account_data.len();
+    let mut is_zero = false;
+    for i in 0..len - 1 {
+        if account_data[i] != 0 {
+            is_zero = false
+        }
+    }
+    is_zero
+}
+
+pub fn bump(seeds: &[&[u8]], program_id: &Pubkey) -> u8 {
+    let (_found_key, bump) = Pubkey::find_program_address(seeds, program_id);
+    bump
+}
